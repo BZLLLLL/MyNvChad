68355fe252ebc6e7e13e5acdbab2fa7efd3b3d34
BZLLLLL <55730497+BZLLLLL@users.noreply.github.com>
2024-07-16 11:33:04 +0800

    Create options.lua

---
+++ lua/options.lua
@@ -0,0 +1,12 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+
+require "configs.autoclose"
+
+-- Change Which-key timeout to 0
+vim.o.timeout = true
+vim.o.timeoutlen = 0
