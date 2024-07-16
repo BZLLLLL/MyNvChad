733cf6a311a5d3a3bef4378c8646c4c1e77ced1c
BZLLLLL <55730497+BZLLLLL@users.noreply.github.com>
2024-07-16 11:31:33 +0800

    Create telescope.lua

---
+++ lua/configs/telescope.lua
@@ -0,0 +1,70 @@
+local options = {
+    defaults = {
+        vimgrep_arguments = {
+            "rg",
+            "-L",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+        },
+        prompt_prefix = "   ",
+        selection_caret = "  ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+                results_width = 0.8,
+            },
+            vertical = {
+                mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+        },
+        file_sorter = require("telescope.sorters").get_fuzzy_file,
+        file_ignore_patterns = { "node_modules" },
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        path_display = { "truncate" },
+        winblend = 0,
+        border = {},
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        color_devicons = true,
+        set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+        mappings = {
+            n = { ["q"] = require("telescope.actions").close },
+        },
+    },
+
+    extensions_list = { "themes", "terms", "projects", "lazy" },
+    extensions = {
+        lazy = {
+            -- Configuration that will be passed to the window that hosts the terminal
+            -- For more configuration options check 'nvim_open_win()'
+            terminal_opts = {
+                relative = "editor",
+                style = "minimal",
+                border = "rounded",
+                title = "Telescope lazy",
+                title_pos = "center",
+                width = 0.7,
+                height = 0.7,
+            },
+        },
+    },
+}
+
+return options
